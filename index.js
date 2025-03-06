2e243e12d944c39795871837684b28fd26cadb8b
y6u7i8o9p0Y&U/I(O)P= <alejodiego536@gmail.com>
2025-03-06 10:11:53 -0500

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const pool = require('./db');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/video_camera', async (req, res) => {
